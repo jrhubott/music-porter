1a836481766ae7e72050eaa13a245436f42f7df7
Jason Rhubottom <jason@rhusoft.com>
2026-02-24 07:32:43 -0800

    Set branch version 2.15.0-list-command

--- porter_core.py
+++ porter_core.py
@@ -45,7 +45,7 @@ def get_os_display_name():
 # Section 1: Constants and Configuration
 # ══════════════════════════════════════════════════════════════════
 
-VERSION = "2.14.0"
+VERSION = "2.15.0-list-command"
 
 DEFAULT_MUSIC_DIR = "music"
 DEFAULT_EXPORT_DIR = "export"
